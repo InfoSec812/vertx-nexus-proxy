ea494d7f9397aef808e2c73cb2891fc1f9a93414
Deven Phillips <deven.phillips@gmail.com>
2015-02-28 19:02:04 -0500

    Initial import

---
+++ src/main/java/com/zanclus/vertx/nexus/proxy/BasicAuthVerticle.java
@@ -0,0 +1,53 @@
+package com.zanclus.vertx.nexus.proxy;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.impl.LoggerFactory;
+import java.util.Base64;
+
+/**
+ *
+ * @author <a href="">Deven Phillips</a>
+ */
+public class BasicAuthVerticle extends AbstractVerticle {
+
+    @Override
+    public void start() throws Exception {
+        Logger log = LoggerFactory.getLogger(this.getClass());
+        final String target = context.config().getString("target_host", "192.168.1.70");
+        final Integer port = context.config().getInteger("target_port", 8081);
+        EventBus eb = vertx.eventBus();
+        eb.consumer("proxy.login.do", (Message<JsonObject> event) -> {
+            log.error("Recieved proxy login event.");
+            String username = event.body().getString("username");
+            String password = event.body().getString("password");
+            JsonObject response = new JsonObject();
+            if (username==null || password==null) {
+                response.put("error", "Username and/or password values are null.");
+                response.put("status", 0);
+                log.error("Sending error response.");
+                eb.send("proxy.login.response", response);
+            } else {
+                String credentials = username+":"+password;
+                String basicAuth = Base64.getEncoder().encodeToString(credentials.getBytes());
+                final String url = String.format("http://%1$s:%2$d/nexus/service/local/user_account/%3$s", target, port, username);
+                vertx
+                    .createHttpClient(new HttpClientOptions())
+                    .request(HttpMethod.GET, url)
+                    .putHeader("Authorization", "Basic "+basicAuth)
+                    .handler(res -> {
+                        response.put("status", res.statusCode());
+                        response.put("response", res.statusMessage());
+                        log.error("Sending login response: "+url+": "+res.statusMessage());
+                        event.reply(response);
+                    })
+                    .end();
+            }
+        });
+    }
+}
---
+++ src/main/java/com/zanclus/vertx/nexus/proxy/Main.java
@@ -0,0 +1,86 @@
+package com.zanclus.vertx.nexus.proxy;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.http.HttpClientRequest;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.impl.LoggerFactory;
+import io.vertx.ext.apex.core.Router;
+import io.vertx.ext.apex.core.RoutingContext;
+
+/**
+ *
+ * @author <a href="">Deven Phillips</a>
+ */
+public class Main extends AbstractVerticle {
+    public static void main(String[] args) {
+        Vertx.vertx().deployVerticle(new Main());
+    }
+
+    /**
+     *
+     * @throws Exception
+     */
+    @Override
+    public void start() throws Exception {
+        Logger log = LoggerFactory.getLogger(this.getClass());
+        if (context.config().getString("rut_header")==null) {
+            context.config().put("rut_header", "REMOTE_USER");
+        }
+        if (context.config().getString("target_host")==null) {
+            context.config().put("target_host", "127.0.0.1");
+        }
+        if (context.config().getString("target_port")==null) {
+            context.config().put("target_port", 8081);
+        }
+        if (context.config().getString("proxy_host")==null) {
+            context.config().put("proxy_host", "127.0.0.1");
+        }
+        if (context.config().getString("proxy_port")==null) {
+            context.config().put("proxy_port", 8080);
+        }
+        vertx.deployVerticle(new BasicAuthVerticle(), res -> {
+            Router router = Router.router(vertx);
+            // router.route("/nexus-proxy/").handler(StaticH)
+            router.routeWithRegex("^/nexus/.*").handler((RoutingContext ctx) -> {
+                ctx.request().headers().remove(context.config().getString("rut_header"));
+                if (HttpMethod.GET.equals(ctx.request().method())) {
+                    String authHeader = ctx.request().headers().get("Authorization");
+                    String type = authHeader.split(" ")[0];
+                    String credentials = authHeader.split(" ")[1];
+                    if (type.toUpperCase().contentEquals("BEARER")) {
+                        // TODO: Verify bearer token and get associated username
+                        // ctx
+                        //    .request()
+                        //    .headers()
+                        //    .add(context.config()
+                        //                .getString("rut_header")
+                        //                .toUpperCase(), "username");
+                    }
+                    String urlWithQueryParams = ctx.request().uri().split("/", 3)[4];
+                    String uri = "http://"
+                                    + context.config().getString("target_host")
+                                    + ":"
+                                    + context.config().getString("target_port")
+                                    + urlWithQueryParams;
+                    HttpClient client = vertx.createHttpClient(new HttpClientOptions());
+                    final HttpClientRequest req = client.request(ctx.request().method(), uri);
+                    req.headers().addAll(ctx.request().headers());
+                    ctx.request().bodyHandler(body -> {
+                        req.end(body.copy());
+                        req.handler(response -> {
+                            ctx.response().headers().addAll(response.headers());
+                            ctx.response().setStatusCode(response.statusCode());
+                            response.bodyHandler(resBody -> {
+                                ctx.response().end(resBody.copy());
+                            });
+                        });
+                    });
+                }
+            });
+        });
+    }
+}
