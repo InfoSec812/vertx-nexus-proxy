fd8666ea79bda47100aa4a5e253113463029db8e
Deven Phillips <deven.phillips@gmail.com>
2015-03-03 22:21:33 -0500

    Cleaned up the lambdas for readability and finished implementing the proxy logic (I think)...

--- src/main/java/com/zanclus/vertx/nexus/proxy/Main.java
+++ src/main/java/com/zanclus/vertx/nexus/proxy/Main.java
@@ -16,6 +16,7 @@ import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.impl.LoggerFactory;
+import io.vertx.core.streams.Pump;
 import io.vertx.ext.apex.Router;
 import io.vertx.ext.apex.RoutingContext;
 import io.vertx.ext.apex.handler.SessionHandler;
@@ -98,6 +99,175 @@ public class Main extends AbstractVerticle {
         });
     }
 
+    /**
+     * Send an event on the event bus to get a user list and reply via HTTP and JSON
+     * @param ctx The {@link RoutingContext} of the request
+     */
+    public void getUserList(RoutingContext ctx) {
+        UserInfo info = this.processAuth((JsonObject)ctx.session().data().get("user_info"));
+        if (info.isAdmin()) {
+            vertx.eventBus().send("proxy.user.list", null, (AsyncResult<Message<JsonObject>> reply) -> {
+                ctx .response()
+                    .setStatusCode(200)
+                    .putHeader("Content-Type", "application/json")
+                    .end(reply.result().body().encodePrettily());
+            });
+        } else {
+            ctx.response().setStatusCode(401).setStatusMessage("Must be admin to list users.").end();
+        }
+    }
+
+    /**
+     * Send an event on the event bus to get a user and reply via HTTP and JSON
+     * @param ctx The {@link RoutingContext} of the request
+     */
+    public void getUser(RoutingContext ctx) {
+        UserInfo info = this.processAuth((JsonObject)ctx.session().data().get("user_info"));
+        String username = ctx.request().params().get("username");
+        if (info.isAdmin() || (info.isAuthenticated() && info.username.contentEquals(username))) {
+            vertx.eventBus().send("proxy.user.list", null, (AsyncResult<Message<JsonObject>> reply) -> {
+                ctx .response()
+                    .setStatusCode(200)
+                    .putHeader("Content-Type", "application/json")
+                    .end(reply.result().body().encodePrettily());
+            });
+        } else {
+            ctx.response().setStatusCode(401).setStatusMessage("Must be admin to view other users.").end();
+        }
+    }
+
+    /**
+     * Send an event on the event bus to delete all of a user's tokens and reply via HTTP and JSON
+     * @param ctx The {@link RoutingContext} of the request
+     */
+    public void deleteUser(RoutingContext ctx) {
+        UserInfo info = this.processAuth((JsonObject)ctx.session().data().get("user_info"));
+        String username = ctx.request().params().get("username");
+        if (info.isAdmin() || (info.isAuthenticated() && info.username.contentEquals(username))) {
+            vertx.eventBus().send("proxy.user.delete", null, (AsyncResult<Message<JsonObject>> reply) -> {
+                ctx .response()
+                    .setStatusCode(200)
+                    .putHeader("Content-Type", "application/json")
+                    .end(reply.result().body().encodePrettily());
+            });
+        } else {
+            ctx.response().setStatusCode(401).setStatusMessage("Must be admin to delete users.").end();
+        }
+    }
+    
+    /**
+     * Send an event on the event bus to delete a user token and reply via HTTP and JSON
+     * @param ctx The {@link RoutingContext} of the request
+     */
+    public void deleteToken(RoutingContext ctx) {
+        UserInfo info = this.processAuth((JsonObject)ctx.session().data().get("user_info"));
+        String username = ctx.request().params().get("username");
+        if (info.isAdmin() || (info.isAuthenticated() && info.username.contentEquals(username))) {
+            JsonObject params = new JsonObject()
+                    .put("username", info.getUsername())
+                    .put("token", ctx.request().params().get("token"));
+            vertx.eventBus().send("proxy.delete.token", params, (AsyncResult<Message<JsonObject>> reply) -> {
+                ctx .response()
+                    .setStatusCode(200)
+                    .putHeader("Content-Type", "application/json")
+                    .end(reply.result().body().encodePrettily());
+            });
+        } else {
+            ctx.response().setStatusCode(401).setStatusMessage("Must be admin to delete tokens from other users.").end();
+        }
+    }
+    
+    /**
+     * Send an event on the event bus to create a user token and reply via HTTP and JSON
+     * @param ctx The {@link RoutingContext} of the request
+     */
+    public void createToken(RoutingContext ctx) {
+        UserInfo info = this.processAuth((JsonObject)ctx.session().data().get("user_info"));
+        String username = ctx.request().params().get("username");
+        if (info.isAdmin() || (info.isAuthenticated() && info.username.contentEquals(username))) {
+            vertx.eventBus().send("proxy.delete.token", username, (AsyncResult<Message<JsonObject>> reply) -> {
+                ctx .response()
+                    .setStatusCode(200)
+                    .putHeader("Content-Type", "application/json")
+                    .end(reply.result().body().encodePrettily());
+            });
+        } else {
+            ctx.response().setStatusCode(401).setStatusMessage("Must be admin to create tokens for other users.").end();
+        }
+    }
+    
+    /**
+     * Make proxy requests to the Nexus server and set the REMOTE_USER header where appropriate.
+     * @param ctx The {@link RoutingContext} of the request
+     */
+    public void proxyNexus(RoutingContext ctx) {
+        // If some nefarious party tried to pass their own REMOTE_USER header, remove it here
+        ctx.request().headers().remove(context.config().getString("rutHeader"));
+        
+        // Only allow REMOTE_USER token auth for GET requests
+        if (HttpMethod.GET.equals(ctx.request().method())) {
+            String authHeader = ctx.request().headers().get("Authorization");
+            String type = authHeader.split(" ")[0];
+            String credentials = authHeader.split(" ")[1];
+            if (type.toUpperCase().contentEquals("BEARER")) {
+                // Verify bearer token and get associated user
+                vertx.eventBus().send("proxy.validate.token", credentials, (AsyncResult<Message<JsonObject>> event) -> {
+                    JsonObject result = event.result().body();
+                    if (result.getString("error")!=null) {
+                        LOG.warn(result.getString("error"));
+                        JsonObject error = new JsonObject()
+                                                    .put("error", Boolean.TRUE)
+                                                    .put("message", result.getString("error"));
+                        ctx.response()
+                                .setStatusCode(401)
+                                .setStatusMessage("Unauthorized: "+result.getString("error"))
+                                .putHeader("Content-Type", "application/json")
+                                .end(error.encodePrettily());
+                    } else {
+                        ctx.request().headers().add(context.config().getString("rutHeader"), result.getString("username"));
+                    }
+                    sendProxyRequest(ctx);
+                });
+            } else {
+                ctx.request().headers().remove(context.config().getString("rutHeader"));
+                sendProxyRequest(ctx);
+            }
+        }
+    }
+
+    /**
+     * Send a request received by the proxy, forward it to the Nexus server, and send back the results
+     * @param ctx The {@link RoutingContext} of the request
+     */
+    private void sendProxyRequest(RoutingContext ctx) {
+        String urlWithQueryParams = ctx.request().uri().split("/", 3)[4];
+        String uri = "http://"
+                + context.config().getString("targetHost")
+                + ":"
+                + context.config().getString("targetPort")
+                + "/" + urlWithQueryParams;
+        HttpClient client = vertx.createHttpClient(new HttpClientOptions());
+        final HttpClientRequest req = client.request(ctx.request().method(), uri);
+        req.headers().clear().addAll(ctx.request().headers());
+        req.endHandler(end -> {
+            ctx.response().end();
+        });
+        req.setChunked(true);
+        req.handler(pResponse -> {
+            ctx.response().headers().clear().addAll(pResponse.headers());
+            ctx.response().setStatusCode(pResponse.statusCode());
+            ctx.response().setStatusMessage(pResponse.statusMessage());
+            ctx.response().setChunked(true);
+            Pump targetToProxy = Pump.pump(pResponse, ctx.response());
+            targetToProxy.start();
+        });
+        Pump proxyToTarget = Pump.pump(ctx.request(), req);
+        proxyToTarget.start();
+    }
+
+    /**
+     * Configure the {@link Router} to route requests to the appropriate handlers.
+     */
     private void configureHttpRequestRouter() {
         // Create a session handler which uses cookies to maintain state across HTTP requests.
         SessionStore store = LocalSessionStore.create(vertx);
@@ -108,138 +278,23 @@ public class Main extends AbstractVerticle {
         
         // Attach the session handler to the Router
         router.route().handler(sessionHandler);
-        
-        // TODO: Configure a route which will server static files
 
-        // Route to handle serving static content for the proxy management web application
+        // Configure the various routes
         router.route(HttpMethod.GET, "/nexus-proxy/").handler(StaticHandler.create());
-        // Route for getting a list of all users
-        router.route(HttpMethod.GET, "/mexus-proxy/api/user").handler(ctx -> {
-            UserInfo info = this.processAuth((JsonObject)ctx.session().data().get("user_info"));
-            if (info.isAdmin()) {
-                vertx.eventBus().send("proxy.user.list", null, (AsyncResult<Message<JsonObject>> reply) -> {
-                    ctx .response()
-                        .setStatusCode(200)
-                        .putHeader("Content-Type", "application/json")
-                        .end(reply.result().body().encodePrettily());
-                });
-            } else {
-                ctx.response().setStatusCode(401).setStatusMessage("Must be admin to list users.").end();
-            }
-        });
-        // Route for getting user details
-        router.route(HttpMethod.GET, "/nexus-proxy/api/user/:username").handler(ctx -> {
-            UserInfo info = this.processAuth((JsonObject)ctx.session().data().get("user_info"));
-            String username = ctx.request().params().get("username");
-            if (info.isAdmin() || (info.isAuthenticated() && info.username.contentEquals(username))) {
-                vertx.eventBus().send("proxy.user.list", null, (AsyncResult<Message<JsonObject>> reply) -> {
-                    ctx .response()
-                        .setStatusCode(200)
-                        .putHeader("Content-Type", "application/json")
-                        .end(reply.result().body().encodePrettily());
-                });
-            } else {
-                ctx.response().setStatusCode(401).setStatusMessage("Must be admin to view other users.").end();
-            }
-        });
-        // Route for DELETEing all of a user's tokens
-        router.route(HttpMethod.DELETE, "/nexus-proxy/api/user/:username").handler(ctx -> {
-            UserInfo info = this.processAuth((JsonObject)ctx.session().data().get("user_info"));
-            String username = ctx.request().params().get("username");
-            if (info.isAdmin() || (info.isAuthenticated() && info.username.contentEquals(username))) {
-                vertx.eventBus().send("proxy.user.delete", null, (AsyncResult<Message<JsonObject>> reply) -> {
-                    ctx .response()
-                        .setStatusCode(200)
-                        .putHeader("Content-Type", "application/json")
-                        .end(reply.result().body().encodePrettily());
-                });
-            } else {
-                ctx.response().setStatusCode(401).setStatusMessage("Must be admin to delete users.").end();
-            }
-        });
-        // Route for DELETEing a single user token
-        router.route(HttpMethod.DELETE, "/nexus-proxy/api/user/:username/:token").handler(ctx -> {
-            UserInfo info = this.processAuth((JsonObject)ctx.session().data().get("user_info"));
-            String username = ctx.request().params().get("username");
-            if (info.isAdmin() || (info.isAuthenticated() && info.username.contentEquals(username))) {
-                JsonObject params = new JsonObject()
-                        .put("username", info.getUsername())
-                        .put("token", ctx.request().params().get("token"));
-                vertx.eventBus().send("proxy.delete.token", params, (AsyncResult<Message<JsonObject>> reply) -> {
-                    ctx .response()
-                        .setStatusCode(200)
-                        .putHeader("Content-Type", "application/json")
-                        .end(reply.result().body().encodePrettily());
-                });
-            } else {
-                ctx.response().setStatusCode(401).setStatusMessage("Must be admin to delete tokens from other users.").end();
-            }
-        });
-        // Route for CREATEing a new user token
-        router.route(HttpMethod.POST, "/nexus-proxy/api/user/:username").handler(ctx -> {
-            UserInfo info = this.processAuth((JsonObject)ctx.session().data().get("user_info"));
-            String username = ctx.request().params().get("username");
-            if (info.isAdmin() || (info.isAuthenticated() && info.username.contentEquals(username))) {
-                vertx.eventBus().send("proxy.delete.token", username, (AsyncResult<Message<JsonObject>> reply) -> {
-                    ctx .response()
-                        .setStatusCode(200)
-                        .putHeader("Content-Type", "application/json")
-                        .end(reply.result().body().encodePrettily());
-                });
-            } else {
-                ctx.response().setStatusCode(401).setStatusMessage("Must be admin to create tokens for other users.").end();
-            }
-        });
-        // Route to proxy requests to the nexus server
-        router.routeWithRegex("^/nexus/.*").handler(ctx -> {
-            ctx.request().headers().remove(context.config().getString("rutHeader"));
-            if (HttpMethod.GET.equals(ctx.request().method())) {
-                String authHeader = ctx.request().headers().get("Authorization");
-                String type = authHeader.split(" ")[0];
-                String credentials = authHeader.split(" ")[1];
-                if (type.toUpperCase().contentEquals("BEARER")) {
-                    // Verify bearer token and get associated user
-                    vertx.eventBus().send("proxy.validate.token", credentials, (AsyncResult<Message<JsonObject>> event) -> {
-                        JsonObject result = event.result().body();
-                        if (result.getString("error")!=null) {
-                            LOG.warn(result.getString("error"));
-                            ctx.request().headers().remove(context.config().getString("rutHeader"));
-                        } else {
-                            ctx.request().headers().add(context.config().getString("rutHeader"), result.getString("username"));
-                        }
-                        sendProxyRequest(ctx);
-                    });
-                } else {
-                    ctx.request().headers().remove(context.config().getString("rutHeader"));
-                    sendProxyRequest(ctx);
-                }
-            }
-        });
+        router.route(HttpMethod.GET, "/mexus-proxy/api/user").handler(this::getUserList);
+        router.route(HttpMethod.GET, "/nexus-proxy/api/user/:username").handler(this::getUser);
+        router.route(HttpMethod.DELETE, "/nexus-proxy/api/user/:username").handler(this::deleteUser);
+        router.route(HttpMethod.DELETE, "/nexus-proxy/api/user/:username/:token").handler(this::deleteToken);
+        router.route(HttpMethod.POST, "/nexus-proxy/api/user/:username").handler(this::createToken);
+        router.routeWithRegex("^/nexus/.*").handler(this::proxyNexus);
         vertx.createHttpServer().requestHandler(router::accept).listen(context.config().getInteger("proxyPort"), context.config().getString("proxyHost"));
     }
 
-    private void sendProxyRequest(RoutingContext ctx) {
-        String urlWithQueryParams = ctx.request().uri().split("/", 3)[4];
-        String uri = "http://"
-                + context.config().getString("targetHost")
-                + ":"
-                + context.config().getString("targetPort")
-                + urlWithQueryParams;
-        HttpClient client = vertx.createHttpClient(new HttpClientOptions());
-        final HttpClientRequest req = client.request(ctx.request().method(), uri);
-        req.headers().addAll(ctx.request().headers());
-        ctx.request().bodyHandler(body -> {
-            req.end(body.copy());
-            req.handler(response -> {
-                ctx.response().headers().addAll(response.headers());
-                ctx.response().setStatusCode(response.statusCode());
-                response.bodyHandler(resBody -> {
-                    ctx.response().end(resBody.copy());
-                });
-            });
-        });
-    }
-
+    /**
+     * Check the user's authentication and return a {@link UserInfo} instance with the authorization information
+     * @param userInfo A {@link JsonObject} containing the Nexus supplied user information.
+     * @return An instance of {@link UserInfo} which is calculated from the user information from Nexus
+     */
     private UserInfo processAuth(JsonObject userInfo) {
         UserInfo info = new UserInfo();
         if (userInfo!=null && userInfo.getJsonObject("data")!=null && userInfo.getJsonObject("data").getString("userId")!=null) {
@@ -256,6 +311,9 @@ public class Main extends AbstractVerticle {
         return info;
     }
 
+    /**
+     * A Bean which contains information about a User
+     */
     private class UserInfo {
         private boolean authenticated = false;
         private boolean admin = false;
